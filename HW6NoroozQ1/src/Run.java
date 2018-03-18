fe52a3f841fee3886fb4581bc02d1a7590b2c918
aliarjomandbigdeli <aliabigdeli@gmail.com>
2018-03-19 02:06:57 +0330

    add feature to reading matrix

--- HW6NoroozQ1/src/Run.java
+++ HW6NoroozQ1/src/Run.java
@@ -5,7 +5,9 @@ public class Run {
     public static void main(String[] args) {
         System.out.println("Run..");
 
-        ArrayList<String> words = new ArrayList<String>();
+        //ArrayList<String> words = new ArrayList<String>();
+        ArrayList<ArrayList<Double>> mat = new ArrayList<ArrayList<Double>>();
+
         int lineCount = 0;
 
 
@@ -21,27 +23,44 @@ public class Run {
             int indexOfComma = 0;
             int nextIndexOfComma = 0;
 
+            ArrayList<Double> row = new ArrayList<>();
+            mat.add(row);
+
             for (int i = 0; i < line.length(); i++) {
                 if (line.charAt(i) == ',') {
                     nextIndexOfComma = i;
-                    String word = "";
+                    String number = "";
                     if (indexOfComma == 0)
-                        word = line.substring(indexOfComma, nextIndexOfComma);
+                        number = line.substring(indexOfComma, nextIndexOfComma);
                     else
-                        word = line.substring(indexOfComma + 1, nextIndexOfComma);
-                    words.add(word);
+                        number = line.substring(indexOfComma + 1, nextIndexOfComma);
+
+                    mat.get(lineCount).add(Double.parseDouble(number));
                 }
                 indexOfComma = nextIndexOfComma;
             }
 
             int lastIndexOfComma = line.lastIndexOf(",");
-            String lastWord = line.substring(lastIndexOfComma + 1);
-            words.add(lastWord);
-
+            String lastNumber = line.substring(lastIndexOfComma + 1);
+            mat.get(lineCount).add(Double.parseDouble(lastNumber));
 
             ++lineCount;
         }
 
+
+
+
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat.get(i).size(); j++) {
+                System.out.print(mat.get(i).get(j) + ", ");
+            }
+            System.out.println();
+        }
+
+
+        String number = "-10";
+        int result = Integer.parseInt(number);
+        System.out.println(result);
 
     }
 }
